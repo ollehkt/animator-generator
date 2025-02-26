ee763b86e3644acd1f615a4836cc493103182e3e
Hyuk <poiu03290@naver.com>
2025-02-26 14:25:41 +0900

    fix: 충돌 수정

--- src/store/index.js
+++ src/store/index.js
@@ -3,16 +3,8 @@ import { useObjectStore } from './objectStore'
 import { useControllerStore } from './controller.store'
 import { usePreviewStore } from './preview.store'
 import { useAnimationStore } from './animation.store'
-import { useProjectsStore } from './project.store'
 import { useViewportStore } from './viewport.store'
+import { useProjectsStore } from './project.store'
 const pinia = createPinia()
 
-export {
-  pinia,
-  useObjectStore,
-  useControllerStore,
-  usePreviewStore,
-  useAnimationStore,
-  useViewportStore,
-  useProjectsStore,
-}
+export { pinia, useObjectStore, useControllerStore, usePreviewStore, useAnimationStore }
